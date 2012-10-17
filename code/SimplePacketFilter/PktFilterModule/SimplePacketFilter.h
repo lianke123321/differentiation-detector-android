b9652229a17da9ef2ef8bcd1c02d9a4dc40809aa
Ashwin <ashwin.rao@inria.fr>
2012-10-16 17:56:51 -0700

    Steps to have 10.x.x.x as the network for the VPN
    Former-commit-id: 1962b407648af86d5e1dfe2f4a31868cd817dd88
    Former-commit-id: 90ba164fa675efe873a6dce97933e362d0e3add5
    Former-commit-id: 1e607dc1d40b4d32ab4628c1d63a6f2be2a27c0c

--- code/SimplePacketFilter/PktFilterModule/SimplePacketFilter.h
+++ code/SimplePacketFilter/PktFilterModule/SimplePacketFilter.h
@@ -19,11 +19,11 @@
 
 //TODO:: Move these to config file
 #define TUN_DEVICE "tun0"
-#define FWD_PATH_NET "192.168.0.0"
-#define REV_PATH_NET "192.168.1.0"
-#define DEV_NETMASK "255.255.254.0"
-#define ROUTE_NETMASK "255.255.255.0"
-#define IP_ADDRESS "192.168.0.1"
+#define FWD_PATH_NET "10.11.0.0" // The IP network of the mobile device just after/before coming from the VPN tunnel
+#define REV_PATH_NET "10.101.0.0" // The IP network for the mobile device after NAT in fwd path
+#define DEV_NETMASK "255.0.0.0" // The netmask used for the tun0 device
+#define ROUTE_NETMASK "255.255.0.0" // The Netmask used for the forward and reverse paths
+#define IP_ADDRESS "10.11.101.101" 
 #define COMMAND_SOCKET_PATH "/data/.meddleCmdSocket"
 
 #define DEFAULT_DNS_SERVER "128.208.4.1"
