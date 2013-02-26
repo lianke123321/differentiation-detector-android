a0d4f76a13f05619961effa00a2f934623de25ee
Amy Tang <amytang0@gmail.com>
2013-02-25 21:47:15 -0800

    Added credit card and emails to grepForStuff
    Former-commit-id: 34adc497120800de16ea391945b154ff9f8adf6a
    Former-commit-id: c0c8d3f01e4df1eb3a96c79ab19fcbef6571089a
    Former-commit-id: dd0996894498db6dfad50f855f2b961d237fc600
    Former-commit-id: 0f5d7930194e7c4b00ebe1eba1b70e9b8fac5e16

--- code/AmyAppAnalysis/scripts/grepForStuff.sh
+++ code/AmyAppAnalysis/scripts/grepForStuff.sh
@@ -2,35 +2,44 @@
 
 filename="$1"
 
-httpdata=$(tcpdump -Ar $filename)
+httpdata=$(tcpdump -Ar $filename| grep -v 'TCP' | grep -v 'HTTP')
 
 echo $httpdata >> httpdata.txt
 
 echo "\nLooking for lat=, latitude=\n"
-grep -o "lat=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "latitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-
+grep -io "lat=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "latitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
 
 echo "\nLooking for lon=, long=, longitude=\n"
-grep -o "lon=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "long=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "longitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "lon=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "long=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "longitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
 
 echo "\nLooking for pw=, pwd=, password=, user=\n"
-grep -o "pw=." httpdata.txt
-grep -o "pwd=." httpdata.txt
-grep -o "password=." httpdata.txt
-grep -o "user=." httpdata.txt
+grep -io "pw=." httpdata.txt
+grep -io "pwd=." httpdata.txt
+grep -io "password=." httpdata.txt
+grep -io "user=." httpdata.txt
 
 echo "\nLooking for IMEI= \n"
-grep -o "IMEI=...." httpdata.txt
+grep -io "IMEI=...." httpdata.txt
 
 echo "\nLooking for phone number, also phone=, number= \n"
-grep -o '......\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' httpdata.txt
-grep -o "number=.........." httpdata.txt
-grep -o "phone=........." httpdata.txt
+grep -io '......\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' httpdata.txt
+grep -io "number=.........." httpdata.txt
+grep -io "phone=........." httpdata.txt
 #grep -i -e -o "lat=\d+\}" httpdata.txt
 
+echo "\nLooking for credit card numbers\n"
+egrep -io '4[0-9]{12}(?:[0-9]{3})?' httpdata.txt #Visa
+egrep -io '5[1-5][0-9]{14}' httpdata.txt #MasterCard
+egrep -io '[47][0-9]{13}' httpdata.txt #AmEx
+egrep -io '3(?:0[0-5]|[68][0-9])[0-9]{11}' httpdata.txt #DinersClub
+egrep -io '6(?:011|5[0-9]{2})[0-9]{12}' httpdata.txt #Discover
+egrep -io '(?:2131|1800|35\d{3})\d{11}' httpdata.txt #JCB
+
+echo "\nLooking for emails\n"
+egrep -io '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}' httpdata.txt
 #tcpdump -Ar $filename | grep -i latitude
 #tcpdump -Ar $filename | grep -i long=
 #tcpdump -Ar $filename | grep -i longitude
