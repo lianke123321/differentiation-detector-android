8c8fa3680bc2030d01e2d16def9ebf852475c7df
Ashwin <ashwin.rao@inria.fr>
2013-03-01 18:27:14 +0100

    renamed webServer to WebServer
    Former-commit-id: 27f1ffc9930c3f54979717c4878f24d834f2488a
    Former-commit-id: c0a79c8582417edf5aa072da52b09f65923df2c3
    Former-commit-id: ad74d8a4a1df775eea706b0596e91949b982adaf
    Former-commit-id: 1be6e63f90e44965711816b2e7783e288f662e71

---
+++ code/SimplePacketFilter/WebServer/MeddleCommunicator.py
@@ -0,0 +1,117 @@
+import socket
+import sys
+import struct
+import logging
+
+from ConfigHandler import configParams
+from StringConstants import *
+
+#TODO:: Add logging support
+
+#define MSG_CREATETUNNEL 1
+#define MSG_CLOSETUNNEL 2
+#define MSG_LOADALLCONFS 3
+#define MSG_GETIPUSERINFO 4
+#define MSG_RESPIPUSERINFO 5
+#define MSG_LOADUSERCONFS 6
+#define MSG_RESPUSERCONFS 7
+MSG_GETIPUSERINFO = 4
+MSG_RESPIPUSERINFO = 5
+
+INET_ADDRSTRLEN = 16
+LEN_HDR = 8
+LEN_USERNAME = 512
+LEN_RESPUSERINFO = LEN_HDR+INET_ADDRSTRLEN + 4 + 4 + LEN_USERNAME
+
+#struct msgIPUserInfo {
+#    uint8_t ipAddress[INET_ADDRSTRLEN];
+#}__attribute__((packed));
+#typedef struct msgIPUserInfo msgGetIPUserInfo_t;
+#struct respIPUserInfo {
+#    uint8_t ipAddress[INET_ADDRSTRLEN];
+#    uint32_t userID;
+#    uint32_t userNameLen;
+#    uint8_t userName[USERNAMELEN_MAX];
+#}__attribute__((packed));
+#typedef struct respIPUserInfo msgRespIPUserInfo_t;
+
+
+class IPUserInfo:
+    ipAddress, userID, userName = None, None, None
+    
+    def __init__ (self, ipAdd, uid, uname):
+        self.ipAddress = ipAdd
+        self.userID = uid
+        self.userName = uname
+    def __str__(self):
+        return "IP:"+str(self.ipAddress)+":ID:"+str(self.userID)+":Name:"+str(self.userName)
+
+class MeddleCommunicator:
+    sock = None
+    sockAddr = None 
+        
+    def __init__ (self):
+        global configParams
+        self.sock = -1;
+        self.sockAddr = (configParams.getParam(MCFG_MSG_SRVIP), int(configParams.getParam(MCFG_MSG_SRVPORT)))
+        logging.error("Connecting to "+str(self.sockAddr))
+         
+    def connectRemoteServer(self):
+        self.sock = socket.socket()#socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            logging.warning("Connecting to "+str(self.sockAddr))
+            self.sock.connect(self.sockAddr)
+            logging.warning("Connected to the Meddle server");
+        except socket.error, msg:
+            logging.error(msg)
+            return False
+        return True  
+                       
+    def __createHeader(self, cmdType, cmdLen):
+        return struct.pack('@II', cmdType, cmdLen) # @ for native byte order 
+    
+    def __createIPUserRequestInfo(self, ipAddress):
+        global MSG_GETIPUSERINFO, INET_ADDRSTRLEN
+        hdr = self.__createHeader(MSG_GETIPUSERINFO, INET_ADDRSTRLEN + 4 + 4);
+        payload = struct.pack('@'+str(INET_ADDRSTRLEN)+'s', ipAddress)
+        return hdr+payload
+        
+    def __getData(self, length):
+        data = ""
+        while (len(data) < length):
+            reqLen = length - len(data)  
+            tmpData = self.sock.recv(reqLen) 
+            data = data + tmpData
+        return tmpData
+ 
+    def requestUserInfo(self, ipAddress):
+        global LEN_RESPUSERINFO, INET_ADDRSTRLEN
+        frame = self.__createIPUserRequestInfo(ipAddress)
+        try:
+            self.sock.send(frame)
+            #print "Sent Frame"         
+            data = self.__getData(LEN_RESPUSERINFO)
+            if data is None:
+                logging.error("Error Getting the Info for user"+str(ipAddress))
+                return None
+            cmdType, cmdLen, ipAddress, userID, userNameLen, userName = struct.unpack('@II'+str(INET_ADDRSTRLEN)+'sII'+str(LEN_USERNAME)+'s',data)
+            return IPUserInfo(ipAddress, userID, userName)
+            #print "IP " +str(ipAddress)+ " User ID"+str(userID)+"userNameLen:"+str(userNameLen)+"userName"+str(userName)             
+        except socket.error, msg:
+            logging.error(msg)
+            return None
+        return None
+    
+    def closeConnection(self):
+        try:
+            self.sock.close()
+        except socket.error, msg:
+            logging.error(msg)
+
+if __name__ == "__main__":
+    #logging.Logger.setLevel(logging.DEBUG)
+    m = MeddleCommunicator();
+    m.connectRemoteServer()
+    ipAddress = "10.11.3.3"
+    print m.requestUserInfo(ipAddress)
+
---
+++ code/SimplePacketFilter/WebServer/UserConfigs.py
@@ -0,0 +1,99 @@
+import tornado.database
+from StringConstants import *
+from ConfigHandler import configParams
+
+class UserConfigs:
+    userName = None
+    userID = None
+    filterAdsAnalytics = None    
+        
+    def __init__(self):
+        self.__updateEntry(None)
+    
+    def __updateEntry(self, dbEntry):
+        if dbEntry is not None:
+            self.userName = dbEntry.userName
+            self.userID = dbEntry.userID
+            self.filterAdsAnalytics = dbEntry.filterAdsAnalytics
+        else:
+            self.userName, self.userID, self.filterAds = None, None, None
+        return
+    
+    def __header(self):        
+        hstr = TEMPLATE_PAGE_HEADER        
+        return hstr
+    
+    def __footer(self):        
+        fstr = TEMPLATE_PAGE_FOOTER
+        return fstr
+    
+    def __htmlAds(self):
+        # 	
+        adStr = "<ul><li>When your phone contacts an ad server, Meddle should "
+        enabled = ""
+        disabled = ""
+        if self.filterAdsAnalytics == 1:
+            enabled = "selected"            
+        else:            
+            disabled = "selected"
+        adStr += """<select name="""+str(CFG_ADS_GRP)+""" onChange="this.form.submit()">"""      
+        adStr += """ <option value = \""""+str(CFG_ADS_GRP_ENABLE_STR)+"""\""""+str(enabled)+""">block ads</option>"""
+        adStr += """ <option value = \""""+str(CFG_ADS_GRP_DISABLE_STR)+"""\""""+str(disabled)+""">allow ads</option>"""
+        adStr += """</select>.</li><ul>"""
+        return adStr
+    
+    def displayConfigs(self):
+        page = ""
+        page += self.__header()
+        page += """<form name="input" action=\""""+str(PAGE_VIEWCONFIGS)+"""\" method="POST">"""
+        page += "Meddle Policy"
+        page += self.__htmlAds()
+        page += """<br/></form>"""
+        #page += """<br/><input type="submit" id=\""""+str(CFG_SUBMIT_ID)+"""\" value=\""""+str(CFG_SUBMIT_STR)+"""\"></form>"""
+        page += self.__footer()
+        page = page.replace(SERVER_HOST_FILLER, configParams.getParam(MCFG_WEBSRV_HOST))
+        page = page.replace(SERVER_PORT_FILLER, configParams.getParam(MCFG_WEBSRV_PORT))
+        return page
+    
+    def fetchConfigs(self, uid):
+        global configParams
+        query = "SELECT * FROM UserConfigs WHERE UserID = "+str(uid)+" ;"
+        dbCon = tornado.database.Connection(host=configParams.getParam(MCFG_DB_HOST), 
+                                            database=configParams.getParam(MCFG_DB_NAME),
+                                            user=configParams.getParam(MCFG_DB_USER), 
+                                            password=configParams.getParam(MCFG_DB_PASSWD))
+        results = dbCon.query(query)
+        if results is not None and len(results) == 1:
+             self.__updateEntry(results[0])
+             return True
+        dbCon.close()
+        return False    
+     
+    def updateAdsConfig(self, ads_string):
+         if ads_string == CFG_ADS_GRP_ENABLE_STR:
+             self.filterAdsAnalytics = 1
+         if ads_string == CFG_ADS_GRP_DISABLE_STR:
+             self.filterAdsAnalytics = 0
+         # If not one of the two then keep unchanged
+         return
+     
+    def commitEntry(self):
+         global configParams
+         query = "UPDATE UserConfigs SET filterAdsAnalytics="+str(self.filterAdsAnalytics)+" WHERE userID="+str(self.userID)+ " ;"
+         dbCon = tornado.database.Connection(host=configParams.getParam(MCFG_DB_HOST), 
+                                            database=configParams.getParam(MCFG_DB_NAME),
+                                            user=configParams.getParam(MCFG_DB_USER), 
+                                            password=configParams.getParam(MCFG_DB_PASSWD))         
+         results = dbCon.execute(query)
+         query = "INSERT INTO UserConfigChange VALUES("+str(self.userID)+", CURRENT_TIMESTAMP, \"filterAdsAnalytics\", "+str(self.filterAdsAnalytics)+");"
+         results = dbCon.execute(query)
+         dbCon.close()
+         return 
+     
+    def __str__(self):
+         return " "+str(self.userName)+ " " +str(self.userID)+" "+str(self.filterAdsAnalytics)
+
+if __name__ == "__main__":
+    u = UserConfigs()
+    u.fetchConfigs(1)
+    print u.displayConfigs()        
