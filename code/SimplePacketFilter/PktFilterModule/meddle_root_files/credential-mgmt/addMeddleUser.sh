0e1b780eb9b54b62dfe0e0e11cd0dad27b6fd219
Ashwin <ashwin.rao@inria.fr>
2013-03-04 18:10:52 +0100

    Scripts to add user and certificates now read their variables from certificate.config
    Former-commit-id: eadbcaa15d7eaa9439945263ee2e98dc69f8dcd1
    Former-commit-id: bc82a5ac7a0cdcf1f5966db01200bab471cf99bb
    Former-commit-id: 70d40c8ec80d83d2af632001c54151cddfc06655
    Former-commit-id: f0def94f2027182a82b54260ec7be0d2b174f0aa

---
+++ code/SimplePacketFilter/PktFilterModule/meddle_root_files/credential-mgmt/addMeddleUser.sh
@@ -0,0 +1,21 @@
+set -a
+source certificate.config
+tmpID=`date +%s | sha1sum | cut -b -10`
+tmpPass=`echo ${tmpID} | cut -b -5`
+clientName="$tmpID"
+clientPassword="$tmpPass"
+
+./gen-Client-P12.sh ${clientName} ${clientPassword}
+
+python genIOSConfigXML.py ${clientName} ${clientPassword} ${mobileConfigOrgName} ${mobileConfigConDisplayName} ${caName} ${mobileConfigServerHostname} ${signingCertsPath}${caCertName} ${clientCertsPath} 
+
+query="INSERT INTO UserConfigs VALUES (0, '${clientName}', 0)"
+mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e ${query}
+query="select * from UserConfigs where UserName=${clientUser}"
+mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e ${query}
+echo "${clientName} : XAUTH ${clientPassword}" >> ${MEDDLE_ETC}/ipsec.secrets
+
+echo "The certificates have been installed in the ${clientCertsPath}"
+echo "For an android device the certificate is ${clientCertsPath}${clientName}.p12"
+echo "For an iOS device the config file ${clientCertsPath}${clientName}.mobileconfig"
+
---
+++ code/SimplePacketFilter/PktFilterModule/meddle_root_files/credential-mgmt/gen-Server-Certs.sh
@@ -0,0 +1,24 @@
+# ASSUMES THAT STRONGSWAN HAS BEEN INSTALLED.
+# First export the variables.
+
+function genServerCerts()
+{
+    ${ipsecBin} pki --gen --outform pem > ${serverKeyName}
+    # Sign a certificate using this key 
+
+    ${ipsecBin} pki --pub --in ${serverKeyName} | ${ipsecBin} pki --issue --cacert ${signingCertsPath}${caCertName} --cakey ${signingCertsPath}${caKeyName} --dn "C=${caCountry}, O=${caOrg}, CN=${serverHostname}" --san="${serverHostname}" --flag serverAuth --flag ikeIntermediate --outform pem > ${serverCertName}
+    # Voila! Now you have a valid certificate
+}
+
+function installServerCerts()
+{
+    # Assumes that file ${SERVER_KEY_NAME} and ${SERVER_CERT_NAME} are present
+    # The CA_CERT needs to be installed in the ca certs file. Note we do not need to install the key in the CA certs file.
+    # The CA_CERT is required to ensure that the server treats this CA as a valid certifying authority
+    mv ${serverCertName} ${MEDDLE_ROOT}/etc/ipsec.d/certs # mv required to keep this directory clean
+    mv ${serverKeyName} ${MEDDLE_ROOT}/etc/ipsec.d/private
+}
+
+source ./certificate.config
+genServerCerts
+installServerCerts
