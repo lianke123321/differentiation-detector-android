f46d3279aed9a5dbce850ab0833f2b2f27569f0b
Ashwin <ashwin.rao@inria.fr>
2013-03-05 17:09:39 +0100

    More text and minor fixes
    Former-commit-id: 30862c24666774b92ea1ec5d57b77e1b6bade064
    Former-commit-id: 95fe05ed5841b38914a407751da8b9c174028a6c
    Former-commit-id: 957f7665aad522403788d69fc2a1c0f56cf44d24
    Former-commit-id: 73b294d8f3f2fff5b6c580130c3b553d15d3cb4c

--- code/SimplePacketFilter/PktFilterModule/meddle_root_files/credential-mgmt/addMeddleUser.sh
+++ code/SimplePacketFilter/PktFilterModule/meddle_root_files/credential-mgmt/addMeddleUser.sh
@@ -5,17 +5,21 @@ tmpPass=`echo ${tmpID} | cut -b -5`
 clientName="$tmpID"
 clientPassword="$tmpPass"
 
-./gen-Client-P12.sh ${clientName} ${clientPassword}
+mkdir -p ${clientCertsPath}
+./genClientP12.sh ${clientName} ${clientPassword}
 
-python genIOSConfigXML.py ${clientName} ${clientPassword} ${mobileConfigOrgName} ${mobileConfigConDisplayName} ${caName} ${mobileConfigServerHostname} ${signingCertsPath}${caCertName} ${clientCertsPath} 
+python genIOSConfigXML.py "${clientName}" "${clientPassword}" "${mobileConfigOrgName}" "${mobileConfigConDisplayName}" "${caName}" "${mobileConfigServerHostname}" "${signingCertsPath}${caCertName}" "${clientCertsPath}" 
 
-query="INSERT INTO UserConfigs VALUES (0, '${clientName}', 0)"
-mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e ${query}
-query="select * from UserConfigs where UserName=${clientUser}"
-mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e ${query}
+query="INSERT INTO UserConfigs VALUES (0, '${clientName}', 0);"
+echo ${query}
+mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e "${query}"
+query="select userID, userName, filterAdsAnalytics from UserConfigs where userName='${clientName}';"
+echo ${query}
+mysql -u ${dbUserName} --password=${dbPassword} -D ${dbName} -e "${query}"
 echo "${clientName} : XAUTH ${clientPassword}" >> ${MEDDLE_ETC}/ipsec.secrets
 
 echo "The certificates have been installed in the ${clientCertsPath}"
 echo "For an android device the certificate is ${clientCertsPath}${clientName}.p12"
 echo "For an iOS device the config file ${clientCertsPath}${clientName}.mobileconfig"
+echo "The installation password is ${clientPassword}"
 
