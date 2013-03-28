3c20c0e7a2ca2902c5856bf3e8423370545bc255
Amy Tang <amytang0@gmail.com>
2013-03-28 11:31:06 -0700

    Made grepForStuff.sh better
    Former-commit-id: 78a1064198cf9372f7e2633a071323b97ef3aade
    Former-commit-id: 9a78ece54842b7a05c9ab1ad5cae8df787c72692
    Former-commit-id: 79778d2204290a387d31848a5958c55828ca0048
    Former-commit-id: b54ccb272c275b05226939515345d6447914fe75

--- code/AmyAppAnalysis/scripts/grepForStuff.sh
+++ code/AmyAppAnalysis/scripts/grepForStuff.sh
@@ -2,44 +2,48 @@
 
 filename="$1"
 
-httpdata=$(tcpdump -Ar $filename| grep -v 'TCP' | grep -v 'HTTP')
-
-echo $httpdata >> httpdata.txt
+tcpdump -Ar $filename| grep -v 'TCP' | grep -v 'HTTP' | grep -v 'seq' >> httpdata.txt
 
 echo "\nLooking for lat=, latitude=\n"
-grep -io "lat=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -io "latitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+egrep -io "(\")?lat(\")?([=:])+\"?[0-9]+(\.[0-9]+)?" httpdata.txt | sort | uniq -c
+egrep -io "(\")?latitude(\")?([=:])+\"?[0-9]+(\.[0-9]+)?" httpdata.txt | sort | uniq -c
 
 echo "\nLooking for lon=, long=, longitude=\n"
-grep -io "lon=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -io "long=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -io "longitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+egrep -io "(\")?lon(\")?([:=])+(\"?)[0-9]+(\.[0-9]+)?" httpdata.txt | sort | uniq -c
+egrep -io "(\")?long(\")?([=:])+(\")?[0-9]+(\.[0-9]+)?" httpdata.txt | sort | uniq -c
+egrep -io "(\")?longitude(\")?([=:])+\"?[0-9]+(\.[0-9]+)?" httpdata.txt | sort | uniq -c
 
 echo "\nLooking for pw=, pwd=, password=, user=\n"
-grep -io "pw=." httpdata.txt
-grep -io "pwd=." httpdata.txt
-grep -io "password=." httpdata.txt
-grep -io "user=." httpdata.txt
+egrep -io "(\")?pw(\")?([=:])+(\"?)....." httpdata.txt | sort | uniq -c
+egrep -io "(\")?pwd(\")?([:=])+(\"?)...." httpdata.txt | sort | uniq -c
+egrep -io "(\")?password(\")?([:=])+(\"?)...." httpdata.txt | sort | uniq -c
+egrep -io "(\")?user(\")?([:=])+(\"?)...." httpdata.txt | sort | uniq -c
 
 echo "\nLooking for IMEI= \n"
-grep -io "IMEI=...." httpdata.txt
-
-echo "\nLooking for phone number, also phone=, number= \n"
-grep -io '......\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' httpdata.txt
-grep -io "number=.........." httpdata.txt
-grep -io "phone=........." httpdata.txt
-#grep -i -e -o "lat=\d+\}" httpdata.txt
+egrep -io "(\")?IMEI(\")?([:=])+(\"?)...." httpdata.txt | sort | uniq -c
+egrep -io "(\")?udid(\")?([:=])+(\"?)[0-9]+" httpdata.txt | sort | uniq -c
+egrep -io "(\")?uuid(\")?([:=])+(\"?)[0-9]+" httpdata.txt | sort | uniq -c
+egrep -io "(\")?-Id(\")?([:=])+(\"?)[0-9]+" httpdata.txt | sort | uniq -c
+#phone-specific searches
+grep -i "9419f52ee69ffcba" httpdata.txt | sort | uniq -c
+grep -i "355031040753366" httpdata.txt | sort | uniq -c
+ 
+eche}} "\nLooking for phone number, also phone=, number= \n"
+grep -io '......([:=])+\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' httpdata.txt | grep -v 'ecr' | grep -v 'seq' | grep -v 'val' | grep -v 'ack' | sort | uniq -c
+egrep -io "(\")?number(\")?([:=])+(\"?).........." httpdata.txt | sort | uniq -c
+egrep -io "(\")?phone(\")?([:=])+(\"?)........." httpdata.txt | sort | uniq -c
+#grep -i -e -o "lat=+\d+\}" httpdata.txt | sort | uniq -c
 
 echo "\nLooking for credit card numbers\n"
-egrep -io '4[0-9]{12}(?:[0-9]{3})?' httpdata.txt #Visa
-egrep -io '5[1-5][0-9]{14}' httpdata.txt #MasterCard
-egrep -io '[47][0-9]{13}' httpdata.txt #AmEx
-egrep -io '3(?:0[0-5]|[68][0-9])[0-9]{11}' httpdata.txt #DinersClub
-egrep -io '6(?:011|5[0-9]{2})[0-9]{12}' httpdata.txt #Discover
-egrep -io '(?:2131|1800|35\d{3})\d{11}' httpdata.txt #JCB
+egrep -io '4[0-9]{12}(?:[0-9]{3})?' httpdata.txt | sort  | uniq -c #Visa
+egrep -io '5[1-5][0-9]{14}' httpdata.txt | sort | uniq -c #MasterCard
+egrep -io '[47][0-9]{13}' httpdata.txt | sort | uniq -c #AmEx
+egrep -io '3(?:0[0-5]|[68][0-9])[0-9]{11}' httpdata.txt | sort | uniq -c #DinersClub
+egrep -io '6(?:011|5[0-9]{2})[0-9]{12}' httpdata.txt | sort | uniq -c #Discover
+egrep -io '(?:2131|1800|35\d{3})\d{11}' httpdata.txt | sort | uniq -c #JCB
 
 echo "\nLooking for emails\n"
-egrep -io '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}' httpdata.txt
+egrep -io '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}' httpdata.txt | grep -v 'png' | grep -v 'jpg' | grep -v 'jpeg' | grep -v 'gif' | sort | uniq -c
 #tcpdump -Ar $filename | grep -i latitude
 #tcpdump -Ar $filename | grep -i long=
 #tcpdump -Ar $filename | grep -i longitude
