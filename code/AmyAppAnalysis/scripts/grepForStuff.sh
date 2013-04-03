4b1881cd2a7add09110f9bf58140e042bce2999b
Justine Sherry <stinie@gmail.com>
2013-04-03 11:35:36 -0700

    adding apks
    Former-commit-id: 6b088d27b92e9353c3ec5c7854e1213955be4306
    Former-commit-id: 344feba286d046735639209bc46e60951d4f1ea1
    Former-commit-id: 8797032a85b84bde3371822004a0f244a3a740a1
    Former-commit-id: 338f611a719de958e3219fcfeb00256132acd062

--- code/AmyAppAnalysis/scripts/grepForStuff.sh
+++ code/AmyAppAnalysis/scripts/grepForStuff.sh
@@ -7,29 +7,36 @@ httpdata=$(tcpdump -Ar $filename)
 echo $httpdata >> httpdata.txt
 
 echo "\nLooking for lat=, latitude=\n"
-grep -o "lat=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "latitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "lat=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "latitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
 
 
 echo "\nLooking for lon=, long=, longitude=\n"
-grep -o "lon=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "long=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
-grep -o "longitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "lon=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "long=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
+grep -io "longitude=.[0-9]\{0,5\}.[0-9]\{0,5\}" httpdata.txt
 
 echo "\nLooking for pw=, pwd=, password=, user=\n"
-grep -o "pw=." httpdata.txt
-grep -o "pwd=." httpdata.txt
-grep -o "password=." httpdata.txt
-grep -o "user=." httpdata.txt
+egrep -io "pw=[^ ]+" httpdata.txt
+egrep -io "pwd=[^ ]+" httpdata.txt
+egrep -io "password=[^ ]+" httpdata.txt
+egrep -io "user=[^ ]+" httpdata.txt
+
+##TODO: add fake password/fake account, and then grep for that pass
+
+echo "\nLooking for email addresses"
+egrep -io "[^ ]+@([a-z]+\.)+(((com)|(org))|((edu)|(net)))" httpdata.txt
 
 echo "\nLooking for IMEI= \n"
-grep -o "IMEI=...." httpdata.txt
+grep -io "IMEI=[0-9]+" httpdata.txt
 
 echo "\nLooking for phone number, also phone=, number= \n"
 grep -o '......\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' httpdata.txt
-grep -o "number=.........." httpdata.txt
-grep -o "phone=........." httpdata.txt
+grep -io "number=.........." httpdata.txt
+grep -io "phone=........." httpdata.txt
 #grep -i -e -o "lat=\d+\}" httpdata.txt
+
+echo "\nLooking for credit card numbers"
 
 #tcpdump -Ar $filename | grep -i latitude
 #tcpdump -Ar $filename | grep -i long=
