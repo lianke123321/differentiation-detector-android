57b8823932bbcd69341b517d99608ac98e32fa75
Ashwin <ashwin.rao@inria.fr>
2013-11-27 16:31:44 +0100

    Code that trigger encryption
    Former-commit-id: 386fee03700984005cec63ce8d0564d145a2454f
    Former-commit-id: 9e56b99ba18a674c867cead05d46a52c49d9b564
    Former-commit-id: 794aa5cd82aee69d14412bd8b59b2c1a27ca48e8
    Former-commit-id: 2f81915ee3c6d2308dded1ddcb63bb0d3dd995ba

--- code/SimplePacketFilter/BroMonitoring/logencryptor.sh
+++ code/SimplePacketFilter/BroMonitoring/logencryptor.sh
@@ -4,4 +4,19 @@ set -a
 source /opt/meddle/meddle.config
 inpName=$1
 dumpName=$2
+isHttp=`echo ${dumpName} | grep "http"`
+echo "${dumpName} ${isHttp}"
+sigLogName=/opt/meddle/logs/assignSignatures.log
+tmpLogName="/opt/meddle/logs/tmp-http.log"
+if [ "${isHttp}" != "" ];
+then
+
+    echo "Assigning Signatures to ${tmpLogName} ${inpName} ${dumpName}" >> ${sigLogName}
+    if [ -r ${inpName} ];
+    then
+        echo "File ${inpName} Exists -- starting analysis" >>  ${sigLogName}
+        cp -f ${inpName} ${tmpLogName}
+        ( /opt/meddle/bro/bin/AssignSignatures.R --args /opt/meddle/meddle.config ${tmpLogName} >> ${sigLogName} 2>&1 & ) &
+    fi
+fi
 ${gpgBinPath} --homedir=${gpgHome} -o ${dumpName} --keyring ${gpgPublicKeyRing} -er ${gpgPublicKeyID} --trust-model always ${inpName}
