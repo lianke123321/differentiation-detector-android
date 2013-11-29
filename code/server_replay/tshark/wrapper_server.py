8d6e352f969286416bc9fdc286ed21f429193923
Arash Molavi <arash@achtung.ccs.neu.edu>
2013-11-29 03:10:34 -0500

    minor changes to server wrapper
    Former-commit-id: f7771c208732577905b313f517ea66c50f3b7e39
    Former-commit-id: fefd69723bb4bbe2f1c6d43339aa66802cef383d
    Former-commit-id: 92b5be0fbdf9dbbfc2365757771bff69ae7f411c
    Former-commit-id: 652026c93d2d805fc6ef4634a0ca014b1f971bcd

--- code/server_replay/tshark/wrapper_server.py
+++ code/server_replay/tshark/wrapper_server.py
@@ -3,40 +3,37 @@ last_dump = ls -lrt | grep 'tcpdump' | tail -1 | awk '{ print $9}'
 '''
 
 import time, os, commands, urllib2
-
+from python_lib import Configs, PRINT_ACTION
 
 
 #print command
 
 def main():
     configs = Configs()
+    configs.set('host', 'ec2-54-243-17-203.compute-1.amazonaws.com')
     configs.set('pcap_folder', '/opt/meddle/pcap-data/')
     configs.set('date_folder', time.strftime('%Y-%b-%d', time.gmtime()))
-    configs.set('phone', 'iphone')
     configs.set('ssh_key', '~/.ssh/meddle')
     configs.set('username', 'ubuntu')
-    configs.set('user', 'dave')
-    configs.set('host', 'ec2-54-243-17-203.compute-1.amazonaws.com')
     
+    configs.set('phone', 'iphone')
+    configs.set('user', 'dave')
+
     configs.set('date_folder', '2013-Oct-15')
     
-    
-    command = ('ssh -i ' + ssh_key + ' ' + username + '@' + host
-              +' \"cd ' + pcap_folder + date_folder + '/' + user + phone
+    command = ('ssh -i ' + configs.get('ssh_key') + ' ' + configs.get('username') + '@' + configs.get('host')
+              +' \"cd ' + configs.get('pcap_folder') + configs.get('date_folder') + '/' + configs.get('user') + configs.get('phone')
               +'; ls -lrt | tail -1\"')
-    file = (commands.getoutput(command)).split(" ")[-1]
+    file = (commands.getoutput(command)).split(" ")[-1]    
+    dir = file.partition('.')[0]
+    os.mkdir(dir)    
+    command = ('scp -i ' + configs.get('ssh_key') + ' ' + configs.get('username') + '@' + configs.get('host') + ':'
+              + configs.get('pcap_folder') + configs.get('date_folder') + '/' + configs.get('user') + configs.get('phone')
+              +'/' + file + ' ' + dir)
+    os.system(command)
     
-    print file
-    print os.path.basename(file)
-    os.path.basename(file)
-    
-#    response = urllib2.urlopen('http://achtung.ccs.neu.edu:8888/get_ip').read()
-#    
-#    
-#    os.system('mkdir kir')
-#    
-#    command = ('scp -i ' + ssh_key + ' ' + username + '@' + host + ':'
-#              + pcap_folder + date_folder + '/' + user + phone
-#              +'/' + file + ' ./kir/')
-#    
-#    os.system(command)
+    client_ip = '1.2.3.4'
+    os.system(('echo ' + client_ip + ' > ' + dir + '/client_ip.txt'))
+
+if __name__=="__main__":
+    main()
