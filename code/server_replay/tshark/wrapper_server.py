4891cf60c7ad09f669268dc6cc3445635fff3dff
arash <arash@ccs.neu.edu>
2013-11-29 02:42:54 -0500

    updated server wrapper
    Former-commit-id: 07b1c849272d2c975a7a6dfec61bdaed8ccf03bc
    Former-commit-id: 745d193e4e3cd18643b82a0e1ddcc600a9da94fe
    Former-commit-id: a015fc5fc20f49dcae5705f96ca3c170392e3f1d
    Former-commit-id: 7296294e943954f53a822286f9bd57e7f6502641

--- code/server_replay/tshark/wrapper_server.py
+++ code/server_replay/tshark/wrapper_server.py
@@ -2,31 +2,41 @@
 last_dump = ls -lrt | grep 'tcpdump' | tail -1 | awk '{ print $9}'
 '''
 
-import time, os, commands
-
-pcap_folder = '/opt/meddle/pcap-data/'
-date_folder = time.strftime('%Y-%b-%d', time.gmtime())
-date_folder = '2013-Oct-15'
-phone = 'iphone'
-ssh_key = '~/.ssh/meddle'
-username = 'ubuntu'
-user = 'dave'
-host = 'ec2-54-243-17-203.compute-1.amazonaws.com'
+import time, os, commands, urllib2
 
 
-command = ('ssh -i ' + ssh_key + ' ' + username + '@' + host
-          +' \"cd ' + pcap_folder + date_folder + '/' + user + phone
-          +'; ls -lrt | tail -1\"')
-file = (commands.getoutput(command)).split(" ")[-1]
-
-print file
-
-os.system('mkdir kir')
-
-command = ('scp -i ' + ssh_key + ' ' + username + '@' + host + ':'
-          + pcap_folder + date_folder + '/' + user + phone
-          +'/' + file + ' ./kir/')
-
-os.system(command)
 
 #print command
+
+def main():
+    configs = Configs()
+    configs.set('pcap_folder', '/opt/meddle/pcap-data/')
+    configs.set('date_folder', time.strftime('%Y-%b-%d', time.gmtime()))
+    configs.set('phone', 'iphone')
+    configs.set('ssh_key', '~/.ssh/meddle')
+    configs.set('username', 'ubuntu')
+    configs.set('user', 'dave')
+    configs.set('host', 'ec2-54-243-17-203.compute-1.amazonaws.com')
+    
+    configs.set('date_folder', '2013-Oct-15')
+    
+    
+    command = ('ssh -i ' + ssh_key + ' ' + username + '@' + host
+              +' \"cd ' + pcap_folder + date_folder + '/' + user + phone
+              +'; ls -lrt | tail -1\"')
+    file = (commands.getoutput(command)).split(" ")[-1]
+    
+    print file
+    print os.path.basename(file)
+    os.path.basename(file)
+    
+#    response = urllib2.urlopen('http://achtung.ccs.neu.edu:8888/get_ip').read()
+#    
+#    
+#    os.system('mkdir kir')
+#    
+#    command = ('scp -i ' + ssh_key + ' ' + username + '@' + host + ':'
+#              + pcap_folder + date_folder + '/' + user + phone
+#              +'/' + file + ' ./kir/')
+#    
+#    os.system(command)
