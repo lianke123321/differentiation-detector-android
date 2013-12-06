c7dc82e8b1a9499127564098becf469dafcece06
Abbas Razaghpanah <abbas@abbas-laptop.(none)>
2013-12-06 02:36:47 -0500

    Added RTT analysis to the script. Cleaned up a bit. (NOTE: now you need to have "tcptrace" and "mono-complete" in order for things to work)
    Former-commit-id: a869480dc32c0d9f8bf749c1c1fe76c74de36407
    Former-commit-id: 269b6bb699c133cd93900f3d63ce5f38e4b03909
    Former-commit-id: a2b8778b5d0cea6a93e3d840bd3bd24f3e7e84a9
    Former-commit-id: b0713a4a07f07728199ed6678a571683b5d78440

--- code/Replay/plot/draw_plots.sh
+++ code/Replay/plot/draw_plots.sh
@@ -17,30 +17,34 @@ rm -rf ../data/text_pcaps
 mkdir ../data/generated_plots
 mkdir ../data/text_pcaps
 
+echo Making the filterer...
 cd filterer
-make
+make >/dev/null
 cd ..
-
-cd rtt
-make
-cd ..
+echo Done.
 
 # For every pcap you find in data/pcaps
 for f in `cd ../data/pcaps;ls *.pcap;cd ../../plot`
 do
     # Filter out the name (strip the extention).
     name=`echo "$f" | cut -d'.' -f1`
+    echo Processing $f...
     # Make the directory for the plot.
     mkdir ../data/generated_plots/$name
+    mkdir ../data/generated_plots/$name/dissected_pcaps
     # Dump the pcap to plain text using tcpdump.
-    tcpdump -nr ../data/pcaps/$f >../data/text_pcaps/$name.txt
-    tshark -r ../data/pcaps/$f -R "tcp.analysis.ack_rtt" -e tcp.analysis.ack_rtt -T fields >../data/generated_plots/$name/$name.RTT
+    tcpdump -nr ../data/pcaps/$f >../data/text_pcaps/$name.txt 2>/dev/null
+    mono SplitCap.exe
     cd ../data/generated_plots/$name
     # Generate the script to draw the plot.
-    echo ../../../plot/filterer/filter ../../text_pcaps/$name.txt `cat ../../confs/"$name"`\; ../../../plot/rtt/rtt ./$name.RTT \>\>meta.txt  >filterit.sh
+    echo ../../../plot/filterer/filter ../../text_pcaps/$name.txt `cat ../../confs/"$name"`\; >filterit.sh
+    cd ../../../plot/splitcap/; mono ./SplitCap.exe -r ../../data/pcaps/$f -o ../../data/generated_plots/$name/dissected_pcaps/ >/dev/null  2>/dev/null; cd ../../data/generated_plots/$name/;
+    echo echo File: `find dissected_pcaps/*.pcap -printf '%s %p\n'|sort -nr|head -n 1|awk '{print $2}'` \>rttstats.txt >>filterit.sh
+    echo tcptrace -lr `find dissected_pcaps/*.pcap -printf '%s %p\n'|sort -nr|head -n 1|awk '{print $2}'`\| awk \'/RTT samples/,/RTT avg:/\' \>\>rttstats.txt\; >>filterit.sh
     chmod +x filterit.sh
     # Run the script.
     ./filterit.sh
     # Go back.
     cd ../../../plot
+    echo Done.
 done
