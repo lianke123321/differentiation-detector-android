98ea729a453cecbd828a4ee4ee3ea7f63eb23bad
Abbas Razaghpanah <abbas@abbas-laptop.(none)>
2013-12-09 16:04:26 -0500

    Removed raw pcaps.
    Former-commit-id: 8a9767580bdbc40a85790b9f3bb09d6d1e03bf50
    Former-commit-id: 8a87b30eeeb496af4fb26244d216202000d0d026
    Former-commit-id: 6b3bd1175d83591366470ab4fc1cc2b56a0c8914
    Former-commit-id: 3718da55c1ccfa79fea04977ff9f782f1197142f

--- docs/mobisys-2014/plots/data/netflix_double/university_nfx/filterit.sh
+++
@@ -1,9 +0,0 @@
-../../../../plot/filterer/filter ../../text_pcaps/university_nfx.txt 100 "Netflix University WiFi";
-echo File: dissected_pcaps/university_nfx.pcap.TCP_10-11-3-3_48978_108-175-35-155_80.pcap >university_nfx.rtt.txt
-tcptrace -lr dissected_pcaps/university_nfx.pcap.TCP_10-11-3-3_48978_108-175-35-155_80.pcap| awk '/RTT min/,/RTT stdev/' >>university_nfx.rtt.txt;
-cat meta.txt >university_nfx.stats.txt;
-tshark -qz io,stat,0.1 -r ../../../pcaps/university_nfx.pcap | ../../../../plot/xput/xput xput.txt >>university_nfx.stats.txt;
-gnuplot xputplot.gp;
-cat university_nfx.rtt.txt | ../../../../plot/rtt/rtt >>university_nfx.stats.txt;
-rm -rf university_nfx.rtt.txt;
-rm -rf meta.txt;
