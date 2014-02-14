e9b7b12517340f6d5bf530874b85cddc91f33894
arash <arash@ccs.neu.edu>
2014-02-13 19:49:28 -0500

    updated simple_udp_server_client.py
    Former-commit-id: 97a883f0af7f3ee8d6e872cf18da8f5756f7b323
    Former-commit-id: bf1b7e141f55647543ce8d25cf4b6ce501ff29c3
    Former-commit-id: 6beaad6452de14bb1f6c8d3808bcb8ec3cf77cd4
    Former-commit-id: 7bc01d06a9889bbd48248d01e939fa97ce8b40c8

--- code/server_replay/tshark/simple_udp_server_client.py
+++ code/server_replay/tshark/simple_udp_server_client.py
@@ -17,20 +17,16 @@ import sys, socket
 from python_lib import Configs, PRINT_ACTION
 
 class Instance():
-    def __init__(self, name, ip, port):
-        self.name = name
+    def __init__(self, ip, port):
         self.ip   = ip
         self.port = port
     def __str__(self):
-        return '\tInstance: {} -- {}-{}'.format(self.name, self.ip, self.port)
-    
-def client(instance):
-    MESSAGE = "Hello, World!"
+        return '\tInstance: {}-{}'.format(self.ip, self.port)
 
-    print "message:", MESSAGE
-    
+def client(instance, message):
+    print "Sending:", message
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(MESSAGE, (instance.ip, instance.port))
+    sock.sendto(message, (instance.ip, instance.port))
 
 def server(instance):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,29 +35,43 @@ def server(instance):
     while True:
         data, addr = sock.recvfrom(4096)
         print "received message:", data
-        
+        if data == 'CloseTheSocket':
+            sock.close()
+            break
+
 def main():
-    
-    instance = Instance('auctung', '129.10.115.141', 55055)
+    ip = '129.10.115.141'
+    ports = [55055, 55056, 55057]
     
     PRINT_ACTION('Reading/setting configs', 0)
     configs = Configs()
+    configs.set('message', 'Hello world!')
     configs.read_args(sys.argv)
     
     configs.check_for(['role'])
     
-    if configs.is_given('ip'):
-        setattr(instance, 'ip', configs.get('ip'))
-    if configs.is_given('port'):
-        setattr(instance, 'port', configs.get('port'))
-        
     configs.show_all()
     print instance
 
     if configs.get('role') == 'server':
-        server(instance)
+        for port in ports:
+            instance = Instance(ip, port)
+            server(instance)
+            instances.append(instance)
     elif configs.get('role') == 'client':
-        server(instance)
+        client(instances[0], '1')
+        client(instances[0], '2')
+        client(instances[1], '1')
+        client(instances[0], '3')
+        client(instances[2], '1')
+        client(instances[1], '2')
+        client(instances[2], '2')
+        client(instances[0], '4')
+        client(instances[1], '3')
+        
+        client(instances[0], 'CloseTheSocket')
+        client(instances[1], 'CloseTheSocket')
+        client(instances[2], 'CloseTheSocket')
     
 if __name__=="__main__":
     main()
