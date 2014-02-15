1648b9d447439fc7dcf67ad652e2943f9f65be38
Arash Molavi <arash@achtung.ccs.neu.edu>
2014-02-14 20:49:04 -0500

    adding modifications to udp_server
    Former-commit-id: c2fefc809e07e115bf8da5ad571d0baae4722ab7
    Former-commit-id: aeb741d412d9248643f061b6d953b4db4a2ac2bd
    Former-commit-id: ddcbf895b07c3d0747f7c719be6b73c3ff88fadb
    Former-commit-id: 6889c53621971777450a7bbffb4070644d744754

--- code/server_replay/tshark/simple_udp_server.py
+++ code/server_replay/tshark/simple_udp_server.py
@@ -5,73 +5,94 @@
 by: Arash Molavi Kakhki (arash@ccs.neu.edu)
     Northeastern University
     
-Goal: this is a simple UDP server-client script for UDP testing
+Goal: this is a simple UDP server script for UDP testing
 
 Usage:
     
-
+ps aux | grep "python simple_udp_server.py" |  awk '{ print $2}' | xargs kill -9
 #######################################################################################################
 #######################################################################################################
 '''
-import sys, socket
-from python_lib import Configs, PRINT_ACTION
+import sys, socket, threading
 
-class Instance():
-    def __init__(self, ip, port):
-        self.ip   = ip
-        self.port = port
-    def __str__(self):
-        return '\tInstance: {}-{}'.format(self.ip, self.port)
+from python_lib import Configs, PRINT_ACTION, UDPset, ServerInstance
 
-def client(instance, message):
-    print "Sending:", message
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(message, (instance.ip, instance.port))
-
-def server(instance):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((instance.ip, instance.port))
+class Server(object):
+    def __init__(self, instance):
+        self.instance = instance
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.instance.ip, self.instance.port))
+        print 'Created server at:', instance
     
-    while True:
-        data, addr = sock.recvfrom(4096)
-        print "received message:", data
-        if data == 'CloseTheSocket':
-            sock.close()
-            break
+    def listen(self):
+        while True:
+            data, address = self.sock.recvfrom(4096)
+            print data, 'at', self.instance.port
+            if data == 'CloseTheSocket':
+                break
+        self.close()
+    #        inQ.append((data, instance.port))
+    
+    def close(self):
+        self.sock.close()
 
+class SideChannel(object):
+    def __init__(self, instance):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((instance.ip, instance.port))
+        self.sock.listen(1)
+
+    def wait_for_sync(self):
+        connection, client_address = self.sock.accept()
+        while True:
+            data = connection.recv(4096)
+            if data == 'Sync':
+                connection.close()
+                break
+            
+    def wait_for_termination(self, servers=[]):
+        connection, client_address = self.sock.accept()
+        while True:
+            data = connection.recv(4096)
+            if data == 'Terminate':
+                print 'Got termination signal. Closing the socket.'
+                connection.close()
+                self.sock.close()
+                break
+    
 def main():
     ip = '129.10.115.141'
     ports = [55055, 55056, 55057]
+    sync_port = 55556
     
-    PRINT_ACTION('Reading/setting configs', 0)
-    configs = Configs()
-    configs.set('message', 'Hello world!')
-    configs.read_args(sys.argv)
+    side_channel = SideChannel(ServerInstance(ip, sync_port, 'SideChannel'))
     
-    configs.check_for(['role'])
+    event   = threading.Event()
+    inQ     = []
+    threads = []
+    servers = []
     
-    configs.show_all()
-    print instance
+    for port in ports:
+        servers.append(Server(ServerInstance(ip, port, str(port))))
+        t = threading.Thread(target=servers[-1].listen)
+        threads.append(t)
+    
+    PRINT_ACTION('Waiting for sync signal...', 0)
+    side_channel.wait_for_sync()
+    PRINT_ACTION('Got it!', 1)
+    
+    for t in threads:
+        t.start()
 
-    if configs.get('role') == 'server':
-        for port in ports:
-            instance = Instance(ip, port)
-            server(instance)
-            instances.append(instance)
-    elif configs.get('role') == 'client':
-        client(instances[0], '1')
-        client(instances[0], '2')
-        client(instances[1], '1')
-        client(instances[0], '3')
-        client(instances[2], '1')
-        client(instances[1], '2')
-        client(instances[2], '2')
-        client(instances[0], '4')
-        client(instances[1], '3')
-        
-        client(instances[0], 'CloseTheSocket')
-        client(instances[1], 'CloseTheSocket')
-        client(instances[2], 'CloseTheSocket')
+    PRINT_ACTION('Waiting for termination signal...', 0)
+    t = threading.Thread(target=side_channel.wait_for_termination, args=[servers])
+    t.start()
+    t.join()
+    
+#    while inQ:
+#        print '\t', inQ[0][0], 'at', inQ[0][1]
+#        inQ.pop(0)
+#        print inQ.pop(0)
     
 if __name__=="__main__":
     main()
