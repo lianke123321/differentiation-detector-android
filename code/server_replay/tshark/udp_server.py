17d9a5273b9a696fe9a02b10c8857c81bcf038b9
Arash Molavi <arash@achtung.ccs.neu.edu>
2014-02-19 21:09:18 -0500

    udp_server.py --> now works behind NAT
    Former-commit-id: 683539fa94963f45b9bfee3554f7e7cac8a1f6c6
    Former-commit-id: bf2783fda7dfa600420a046980a17a7532d9e145
    Former-commit-id: 36bf2203bee3a0aa0f78ea7172ac28040a7347d5
    Former-commit-id: 493fc64d5f2d59a0a0e2d686ba3fcc6c22909bc7

--- code/server_replay/tshark/udp_server.py
+++ code/server_replay/tshark/udp_server.py
@@ -9,7 +9,7 @@ Goal: this is a simple UDP server script for UDP testing
 
 Usage:
     
-ps aux | grep "python simple_udp_server.py" |  awk '{ print $2}' | xargs kill -9
+ps aux | grep "python udp_server.py" |  awk '{ print $2}' | xargs kill -9
 #######################################################################################################
 #######################################################################################################
 '''
@@ -24,20 +24,30 @@ class UDPServer(object):
         self.sock.bind((self.instance.ip, self.instance.port))
         print 'Created server at:', instance
             
-    def wait_for_client(self, mapping, Qs, event, pipe_list):
+    def wait_for_client(self, mapping, Qs, queue):
         while True:
             data, client_address = self.sock.recvfrom(4096)
             print "{} from {} @ {}".format(data, client_address, self.instance.port)
             
+            client_ip   = client_address[0]
+            client_port = client_address[1]
+            
+            if client_ip not in mapping:
+                print 'NEW IP:', client_ip
+                mapping[client_ip] = {}
+            
             try:
-                c_s_pair = mapping[client_address[0]][str(client_address[1])]
-                pipe_p, pipe_ch = multiprocessing.Pipe()
-                pipe_list.append(pipe_p)
-                p = multiprocessing.Process(target=self.send_Q(Qs[c_s_pair], time.time(), client_address, pipe_ch, event))
+                c_s_pair = mapping[client_ip][client_port]
+                if c_s_pair is None:
+                    pass
+                mapping[client_ip][client_port] = None
+                p = multiprocessing.Process(target=self.send_Q(Qs[c_s_pair], time.time(), client_address, queue))
                 p.start()
-                del mapping[client_address[0]][str(client_address[1])]
-            except:
-                pass
+            except KeyError:
+                print 'New port:', client_address
+                mapping[client_ip][client_port] = data
+                queue.put(client_address)
+                print  queue.qsize()
                         
     def receive(self):
         while True:
@@ -46,7 +56,7 @@ class UDPServer(object):
             if data == 'CloseTheSocket':
                 break
     
-    def send_Q(self, Q, time_origin, client_address, pipe_ch, event):
+    def send_Q(self, Q, time_origin, client_address, queue):
         if time.time() < time_origin + Q.starttime:
             time.sleep((time_origin + Q.starttime) - time.time())
         
@@ -57,8 +67,8 @@ class UDPServer(object):
             self.sock.sendto(udp_set.payload, client_address)
             print '\tSent', udp_set.payload, 'to', client_address
         
-        pipe_ch.send(client_address)
-        event.set()
+        print '\n\nDONE WITH:', client_address, '\n\n'
+        queue.put(client_address)
         
     def terminate(self):
         self.sock.close()
@@ -71,64 +81,28 @@ class SideChannel(object):
         self.sock.bind((instance.ip, instance.port))
         self.sock.listen(1)
         self.connection_map = {}
-        
-    def wait_for_mapping(self, mapping):
+        self.queue = None
+    
+    def run(self, queue):
+        t1 = threading.Thread(target=self.wait_for_connections)
+        t2 = threading.Thread(target=self.notify, args=(queue,))
+        t1.start()
+        t2.start()
+    
+    def notify(self, queue):
+        while True:
+            print 'Waiting to read from queue...'
+            data      = queue.get()
+            client_ip = data[0]
+            port      = data[1]
+            print 'Sending', port, 'TO', client_ip
+            self.connection_map[client_ip].sendall(str(port))
+            
+    def wait_for_connections(self):
         while True:
             connection, client_address = self.sock.accept()
             self.connection_map[client_address[0]] = connection
-            t = threading.Thread(target=self.handle_connection, args=[connection, client_address, mapping])
-            t.start()
-    
-    def notify_send_done(self, client_address):
-        client_ip   = client_address[0]
-        client_port = client_address[1]
-        self.connection_map[client_ip].sendall(str(client_port))
-    
-    def handle_connection(self, connection, client_address, mapping):
-        client_ip = client_address[0]
-        data = connection.recv(3)
-
-        if data == 'Fin':
-            print 'Got Fin!'
-            self.terminate()
-            sys.exit()
-            return
-        
-        elif data == 'New':
-            mapping[client_ip] = {}
-        
-        while True:
-            data = connection.recv(49)
-            if data == 'Done':
-                print 'got termination'
-                break
-            data = data.split(';')
-            c_s_pair = data[0]
-            port     = data[1]
-            print 'got identification:', data
-            mapping[client_ip][port] = c_s_pair
-        print mapping
-    
-    def check_pipes(self, pipe_list, event):
-        '''
-        Every time send_Q is done, the respective client needs to be notified
-        so it can close the process which is receiving data. To do so:
-        
-        When send_Q() is done, it sends the client_address = (ip, port) into
-        a pipe and sets an event. The event causes this method to wake up, select
-        ready pipes, and notify respective client[s] to terminate corresponding 
-        receiving processes. 
-        '''
-        while True:
-            event.wait()
-            r, w, e = select.select(pipe_list, [], [])
-            for pipe in r:
-                client_address = pipe.recv()
-                self.notify_send_done(client_address)
-                pipe.close()
-                pipe_list.remove(pipe)
-            event.clear()
-    
+            
     def terminate(self):
         self.live = False
         self.sock.close()
@@ -170,8 +144,9 @@ def create_test_Qs(ports):
 
 def main():
     PRINT_ACTION('Creating test Qs', 0)
-    ip = '129.10.115.141'
     ip = '127.0.0.1'
+    ip = '129.10.115.141'
+    ip = ''
     sidechannel_port = 55555
     
     Qs, ports = create_test_Qs(ip)
@@ -183,27 +158,23 @@ def main():
              Every time a connection comes in, mapping[client_ip] is set to {},
              then the client keeps identifying what port is for what c_s_pair
     ########################################################################### 
-    '''
-    PRINT_ACTION('Creating side-channel', 0)
-    mapping   = {}
-    event     = threading.Event()
-    pipe_list = []
-    
-    side_channel = SideChannel(SocketInstance(ip, sidechannel_port))
-    t1 = threading.Thread(target=side_channel.wait_for_mapping, args=[mapping])
-    t2 = threading.Thread(target=side_channel.check_pipes, args=[pipe_list, event])
-    t1.start()
-    t2.start()
-
+    '''    
     PRINT_ACTION('Creating servers', 0)
-    threads   = []
-    servers   = []
+    threads = []
+    servers = []
+    mapping = {}
+    queue   = multiprocessing.Queue()
     
     for port in ports:
         servers.append(UDPServer(SocketInstance(ip, port)))
-        t = threading.Thread(target=servers[-1].wait_for_client, args=[mapping, Qs, event, pipe_list])
+        t = threading.Thread(target=servers[-1].wait_for_client, args=[mapping, Qs, queue])
         t.start()
         threads.append(t)
+    
+    PRINT_ACTION('Creating side-channel', 0)
+    
+    side_channel = SideChannel(SocketInstance(ip, sidechannel_port))
+    side_channel.run(queue)
     
 if __name__=="__main__":
     main()
