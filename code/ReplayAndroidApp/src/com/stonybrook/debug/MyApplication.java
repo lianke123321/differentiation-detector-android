49421dc88eae6033cdf10e1ac6d4e921e76aced9
ankeli-laptop <ankeli@cs.stonybrook.edu>
2015-02-26 13:19:42 -0500

    changed acra to send reports to server silently. this does not work all the time

--- code/ReplayAndroidApp/src/com/stonybrook/debug/MyApplication.java
+++ code/ReplayAndroidApp/src/com/stonybrook/debug/MyApplication.java
@@ -1,14 +1,16 @@
 package com.stonybrook.debug;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
 
 import android.app.Application;
 
 import com.stonybrook.replay.R;
 
-/*@ReportsCrashes(
+@ReportsCrashes(
 	    formUri = "https://ankeli.cloudant.com/acra-diffdetector/_design/acra-storage/_update/report",
 	    reportType = HttpSender.Type.JSON,
 	    httpMethod = HttpSender.Method.POST,
@@ -25,12 +27,12 @@ import com.stonybrook.replay.R;
 	            ReportField.STACK_TRACE
 	    },
 	    mode = ReportingInteractionMode.TOAST,
-	    resToastText = R.string.crash_toast_text)*/
+	    resToastText = R.string.crash_toast_text)
 
-@ReportsCrashes(//formKey = "", // will not be used
+/*@ReportsCrashes(//formKey = "", // will not be used
 				mailTo = "diffdetector.report@ankeli.me",
 				mode = ReportingInteractionMode.TOAST,
-				resToastText = R.string.crash_toast_text)
+				resToastText = R.string.crash_toast_text)*/
 
 public class MyApplication extends Application {
 	@Override
